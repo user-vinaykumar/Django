0eb62e60ec36d3b999a3de224617d480806a96db
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-12 18:15:57 +0530

    created url page for urls

--- myApp/blog/url.py
+++ myApp/blog/url.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('login/', views.login, name='blog-login'),
+    path('payment/', views.payment, name='blog-payment')
 ]
